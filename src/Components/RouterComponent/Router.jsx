b138cea08f3e628f160e09a3f713b3b625b22491
MaxMadeleine <Max.Madeleine.G@gmail.com>
2025-02-05 08:33:25 +0100

    refactor: reorganize user component files and implement EditUser functionality

--- src/Components/RouterComponent/Router.jsx
+++ src/Components/RouterComponent/Router.jsx
@@ -4,17 +4,12 @@ import { HomePage } from "../../PageComponents/HomePageComponent/HomePage.jsx";
 import { CreateUser } from "../../Components/CreateUserComponent/CreateUser.jsx";
 import { EditUser } from "../../Components/EditUserComponent/EditUser.jsx";
 
-
-
 export const Router = () => {
-
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="create/user" element={<CreateUser />} />
-      <Route path="user/:id/edit" element={<EditUser />} />
-
-
-    </Routes>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="create/user" element={<CreateUser />} />
+        <Route path="user/:id/edit" element={<EditUser />} />
+      </Routes>
   )
 }
